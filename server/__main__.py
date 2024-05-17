fb2ae8d3a137680735c7d345d90b4d1553b0902a
jonathan-haubrich <spacecowboy4@gmail.com>
2024-05-17 12:35:11 -0400

    fix cors

--- server/__main__.py
+++ server/__main__.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from .routers import exercises, containers
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(exercises.router)
 app.include_router(containers.router)
